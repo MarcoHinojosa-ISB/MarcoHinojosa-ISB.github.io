6737f3e716b97410916effbe74f81d9fbe0aff56
islaybears <marcndanny@gmail.com>
2018-07-30 00:55:47 -0500

    added work history

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
-$(document).ready(function(){
+function showContent(e, target){
+  var tabContent = document.getElementsByClassName("tab-content");
 
-})
+  for(var i=0; i<tabContent.length; i++){
+    tabContent[i].style.display = "none";
+  }
+
+  var tabLinks = document.getElementsByClassName("tab-links");
+
+  for(var i=0; i<tabLinks.length; i++){
+    tabLinks[i].className = tabLinks[i].className.replace(" active", "");
+  }
+
+  document.getElementById(target).style.display = "block";
+  e.currentTarget.className += " active";
+}
